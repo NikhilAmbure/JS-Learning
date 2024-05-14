45e3d1efedb4e9c064734bb69dd33a194be528ac
Nikhil_A <nikhilambure25@gmailcom>
2024-05-14 15:43:14 +0530

    Make Add to Cart button interactive

--- Recap-2/Amazon clone/scripts/amazon.js
+++ Recap-2/Amazon clone/scripts/amazon.js
@@ -1,38 +1,5 @@
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090 // price in cents
-
-    },
-
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4.0,
-            count: 127
-        },
-        priceCents: 2095
-    },
-
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    }
-];
-
 // Generating html using above array
-
+// Array from the products.js
 let productsHTML = '';
 
 products.forEach((product)=>{
@@ -80,7 +47,8 @@ products.forEach((product)=>{
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id = "${product.id}">
           Add to Cart
         </button>
       </div>
@@ -88,6 +56,46 @@ products.forEach((product)=>{
 });
 
 console.log(productsHTML);
+
 // Now put the html into the webpage
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+
+
+
+// Add to Cart - adding eventlistener
+// Data Attribute : - Is just another HTML attribute
+//  - Allows us to attach any info of an element
+// Syntax for data attribute : - have to start with "data-" 
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+    button.addEventListener('click', ()=>{
+    //    console.log(button.dataset); 
+    // from line 50****
+    //    console.log(button.dataset.productName);  
+    //    Dataset - adds the all data attributes
+    const productId = button.dataset.productId;
+    // productId for uniqueness for an each product
+    
+    let matchingItem; 
+
+    cart.forEach((item)=>{
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+
+    
+    console.log(cart);
+    });
+});
