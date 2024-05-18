300780ba81e1710044b09c6aac1fc9721b9dfb6d
Nikhil_A <nikhilambure25@gmailcom>
2024-05-18 16:13:19 +0530

    Save cart in localstorage

--- Recap-2/Amazon clone/data/cart.js
+++ Recap-2/Amazon clone/data/cart.js
@@ -1,12 +1,23 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
-    }, {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1 
-    }
-];
+// JSON.parse to convert that string into array
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2
+        }, {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1 
+        }
+    ];
+}
+
+// function to create a localstorage- it doesn't get reset when we refresh
+// localstorage only gets the string so to convert into string => JSON.stringify(cart)
+export function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Add to Cart - adding eventlistener
 // Data Attribute : - Is just another HTML attribute
@@ -36,6 +47,8 @@ export function addToCart(productId) {
         quantity
       });
     }
+
+    saveToStorage();
   }
 
 export function removeFromCart(productId) {
@@ -48,4 +61,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
